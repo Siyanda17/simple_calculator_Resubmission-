bc709473c40bee95412efc71e49c2be48853a6d0
Siyanda Dindi <jeanclaudesiyanda@gmail.com>
2020-05-04 10:17:11 +0200

    Removing my_jasmine goodies directory on github

--- my_jasmine_goodies/src/simple_calculator.js
+++
@@ -1,9 +0,0 @@
-function add(...args){
-    return args.reduce((previousValue, currentValue) => previousValue + currentValue)
-}
-
-function multiply(...args){
-    return args.reduce((previousValue, currentValue) => previousValue * currentValue)
-}
-//console.log(add(1,5,8)+" "+multiply(5,6))
-module.exports = {add,multiply}
